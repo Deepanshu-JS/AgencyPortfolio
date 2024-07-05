9138d1d45a6dc426b47afcfefaaaef19f6d5141a
Deepanshu Singh <singhdepanshu786@gmail.com>
2024-07-05 18:49:41 +0530

    Loader Page Complete

---
+++ script.js
@@ -0,0 +1,66 @@
+
+// Loader Code
+function Createloader() {
+  var tl = gsap.timeline();
+
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.3,
+  });
+  
+  // Loader Counting Code
+  tl.from("#line-part1, .line h2", {
+    opacity: 0,
+    onstart: function () {
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
+      setInterval(function () {
+        if (grow < 100) {
+          h5timer.innerHTML = grow++;
+        } else {
+          h5timer.innerHTML = grow;
+        }
+      }, 15);
+    },
+  });
+
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 2,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+
+  
+  // Page 1 Code
+  tl.from("#page1", {
+    y: 1200,
+    delay: 0.5,
+    opacity: 0,
+    duration: 0.5,
+  });
+
+  // page 1 animation
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.1,
+  })
+}
+
+// Mouse Code And page1 Element Magnet code
+function MouseCode(){
+  document.addEventListener("mousemove", function(dets){
+    gsap.to(".crsr",{
+     left:dets.x,
+     top:dets.y
+    })
+ });
+ 
+ 
+ Shery.makeMagnet("#nav-part2 h4, #svg",{});
+}
+// function Call
+Createloader();
+MouseCode();
